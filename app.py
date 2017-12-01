b66c27974fcdccbe52c32c91656ecea6de143f31
Brianojee <brian@codeinstitute.net>
2017-12-01 11:20:30 +0000

    created add task form ( excluding select and submit)

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb://admin:admin@ds123926.mlab.com:23926/task_manager'
 
-@app.route('/')
-def hello():
-    return "Hello World .. again "
+mongo = PyMongo(app)
+
+
+@app.route('/get_tasks')
+def get_tasks():
+    _tasks = mongo.db.tasks.find()
+    task_list = [task for task in _tasks]
+    return render_template('tasks.html', tasks = task_list)
+    
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
+
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
