39dee8b652071bf31735dab73057a04e7218697c
Brianojee <brian@codeinstitute.net>
2017-12-11 13:32:54 +0000

    06 Added add_task form 02

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ def get_tasks():
     return render_template("tasks.html", 
     tasks=mongo.db.tasks.find())
     
-
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
