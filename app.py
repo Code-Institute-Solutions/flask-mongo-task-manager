396096077441068c77624f577bd065ace665a38c
Brianojee <brian@codeinstitute.net>
2017-12-11 14:33:50 +0000

    08 add_task submit 01

--- app.py
+++ app.py
@@ -22,6 +22,13 @@ def add_task():
     categories=mongo.db.categories.find())
     
     
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks =  mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+    
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
