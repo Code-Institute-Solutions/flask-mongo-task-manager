c01f9e80740da5e1a17bb5deffdd547546ca6f3a
Brianojee <brian@codeinstitute.net>
2017-12-12 08:05:38 +0000

    15 edit category 01

--- app.py
+++ app.py
@@ -53,16 +53,27 @@ def update_task(task_id):
 def delete_task(task_id):
     mongo.db.tasks.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_tasks'))
+    
 
 @app.route('/get_categories')
 def get_categories():
     return render_template('categories.html',
     categories=mongo.db.categories.find())
+    
 
-@app.route('/delete_category/<category_id>')
-def delete_category(category_id):
-    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+    category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+
+    
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get['category_name']})
     return redirect(url_for('get_categories'))
+   
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
