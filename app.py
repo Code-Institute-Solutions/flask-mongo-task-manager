b6f3bf6c6ceb4c06aefa4fdac66c0b476e79341b
Brianojee <brian@codeinstitute.net>
2017-12-12 08:36:15 +0000

    18 new category 01

--- app.py
+++ app.py
@@ -73,7 +73,27 @@ def update_category(category_id):
         {'_id': ObjectId(category_id)},
         {'category_name': request.form.get['category_name']})
     return redirect(url_for('get_categories'))
-   
+    
+
+@app.route('/delete_category/<category_id>')  
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for("get_categories"))
+    
+
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    categories = mongo.db.categories
+    category_doc = {'category_name': request.form.get['category_name']}
+    categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+    
+
+@app.route('/new_category')
+def new_category():
+    return render_template('addcategory.html')
+
+       
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
